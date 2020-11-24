9824f2040e8501f4806629a1f24cec3de10f7302
MERCYmsr <74905464+MERCYmsr@users.noreply.github.com>
2020-11-24 18:31:47 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,19 @@
+number_of_wheels=2
+print (number_of_wheels)
+0
+a,b,c=10,20,30
+print(a/10)
+print(b*50)
+print(c+60)
+
+str="India is the developing country"
+print(str.replace("developing","developed"))
+
+a,b=10,11.0
+
+print(float(a))
+print(int(b))
+
+
+
+
