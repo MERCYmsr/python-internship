3b61086aaeab2718bfbade273187c633f957a816
MERCYmsr <74905464+MERCYmsr@users.noreply.github.com>
2020-11-24 14:31:02 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,49 @@
+print("hello world")
+print("30 days 30 hour challenge")
+print('30 days 30 hour challenge')
+hours="thirty"
+print(hours)
+
+Days="thirty days"
+print(Days[1])
+0
+challenge="i'll win"
+print(challenge[7:10])
+sports="i'm the winner of the match"
+print(sports[8:10])
+mercy=("captain of the team")
+print(mercy[1])
+mercy=("captain of the team")
+print(mercy[2:5])
+print(len(mercy))
+print(mercy.lower())
+print(mercy.upper())
+a="30 days"
+b="30 hours"
+c=a+b
+print(c)
+a="30 days"
+b="30 hours"
+c=a+" "+b
+print(c)
+text="python intern thirty days thirty hours"
+x=text.casefold()
+print(x)
+text="python intern thirty days thirty hours"
+x=text.swapcase()
+print(x)
+text="Python Intern Thirty Days Thirty Hours"
+x=text.casefold()
+print(x)
+swap=("swap the value")
+print(swap[2:])
+
+
+
+
+
+
+
+
+
+
