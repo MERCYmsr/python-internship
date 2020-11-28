174e5e563cd0a53733f6a251f34bc5909e5b904e
MERCYmsr <74905464+MERCYmsr@users.noreply.github.com>
2020-11-28 19:39:17 +0530

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,33 @@
+def myfun(num):
+    num[0]=20
+    list=[1,2,3,4,5]
+    myfun(list)
+    print(list)
+
+def myfun():
+    print("hello from a function")
+
+def csk(fname):
+    print("player name is"+fname)
+
+    csk("dhoni")
+    csk("raina")
+    csk("sam")
+
+a = int(input())
+b = int(input())
+
+def arithmetic(X,Y):
+    print("addition of two numbers",X+Y)
+    print("sub of the 2 numbers",X-Y)
+    print("div of 2 numbers",X/Y)
+    print("mul of 2 numbers",X*Y)
+arithmetic(a,b)
+0
+
+
+
+
+
+
+
---
+++ task5b.py
@@ -0,0 +1,4 @@
+def covid(patient_name,temp=98):
+    print("Enter patient name",patient_name)
+    print("Enter body temp is",temp,"degree")
+covid("pratyush")
